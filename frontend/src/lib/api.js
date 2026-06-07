5978192416e0a58d73bd96af90df1d7a54a29c3b
anywork19 <nabeel.ucp@gmail.com>
2026-06-07 11:49:05 +0100

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://anywork-nygr.onrender.com';
 const API = `${BACKEND_URL}/api`;
 
 // Create axios instance
